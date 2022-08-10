88bd55d710d944b23ff7c234004eb154c01f0a84
Bernardo <eduardo.bernardo@indexdc.com>
2022-08-10 19:52:13 -0400

    Analytics e JivoChat

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 import '../styles/globals.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
   <>
+    <Script strategy="lazyOnload" id="Analytics" src={`https://www.googletagmanager.com/gtag/js?id=UA-235363606-1`} />
+    <Script strategy="lazyOnload" id="JivoChat" src={`//code.jivosite.com/widget/0eylLUOEWX`} />
+    <Script strategy="lazyOnload" id="Analytics-1">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-235363606-1', {
+            page_path: window.location.pathname,
+            });
+        `}
+    </Script>
     <Component {...pageProps} />
   </>
   )
