dc7c2efca6fb4453f538855f281dc0da977c4309
Bernardo <eduardo.bernardo@indexdc.com>
2022-08-22 10:38:01 -0400

    Inserção do Pixel Meta

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,21 @@ function MyApp({ Component, pageProps }) {
             });
         `}
     </Script>
+    <Script strategy="lazyOnload" id="Meta-Pixel">
+      {`!function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1495444717580179');
+      fbq('track', 'PageView');`}
+    </Script>
+    <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=1431457984030108&ev=PageView&noscript=1"
+    /></noscript>
     <Component {...pageProps} />
   </>
   )
